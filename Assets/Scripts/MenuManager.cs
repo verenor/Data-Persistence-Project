ce9ddc56e5db72ba83535796d574e48fce1ea12a
verenor <andre.klingspohn@gmail.com>
2025-05-03 07:24:04 +0200

    added menu functionality

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,37 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("main");
+    }
+
+    public void EndGame()
+    {
+        SceneManager.LoadScene("startMenu");
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit(); // original code to quit Unity player
+#endif
+    }
+}
